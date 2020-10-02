73fc4742943d9ee585288ed924fbca74bce1ccd7
ritathesaver <ritathesaver@gmail.com>
2020-10-02 19:42:39 +0300

    create logger

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,4 +1,5 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import rootReducer from './rootReducer'
+import { logger } from './middlewares/logger'
 
-export const store = createStore(rootReducer)
+export const store = createStore(rootReducer, applyMiddleware(logger))
